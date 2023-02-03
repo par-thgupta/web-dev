30b5ac6f1f00144652afbbc1eba5c1c7d23f75c2
Parth Gupta <95039857+par-thgupta@users.noreply.github.com>
2023-02-03 19:19:52 +0530

    calculator

--- html/test2.cpp
+++ html/test2.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-void call(string &s,int &n,int i,string help){
-    if(i == n){
-        cout<<help<<" ";
-        return ;
+void call(string &s, int &n, int i, string help)
+{
+    if (i == n)
+    {
+        cout << help << " ";
+        return;
     }
-    
-    call(s,n,i+1,help+s[i]);
-    call(s,n,i+1,help);
-
-
+    call(s, n, i + 1, help + s[i]);
+    call(s, n, i + 1, help);
 }
-int main(){
-    string s;cin>>s;
+int main()
+{
+    string s;
+    cin >> s;
     int n = s.size();
-    call(s,n,0,"");
-return 0;
+    call(s, n, 0, "");
+    return 0;
 }
---
+++ js/calculation.js
@@ -0,0 +1,39 @@
+console.log("connected");
+
+
+
+let item = document.getElementsByClassName('item');
+
+for (let i of item) {
+    i.addEventListener('click', (text) => {
+        let input = text.target.innerText;
+        let inputtest = document.getElementById("inputID");
+        if (input == 'AC') {
+            inputtest.value = "";
+            return;
+        }
+        else if (input == 'Delete') {
+            let newinpLength = inputtest.value.length;
+            let newinp = inputtest.value.slice(0, newinpLength - 1);
+            inputtest.value = newinp;
+            return;
+        }
+        else if (input == 'X') {
+            input = '*';
+        }
+        else if (input == '&#247;') {
+            input = '/';
+        }
+        else if (input == 'R') {
+            alert("Developed by : THE PARTH")
+            return;
+        }
+        else if (input == 'ANS') {
+            let ans = eval(inputtest.value);
+            inputtest.value = ans;
+            console.log(ans);
+            return;
+        }
+        inputtest.value += input;
+    });
+}
